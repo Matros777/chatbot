f3b09e5d75cd1c6cc0aac42821da1d0cfd59b4a9
Matros777 <matros@github.com>
2026-09-07 05:57:12 +0300

    fix: proxy matcher excludes icons and og images (no login redirect)

--- proxy.ts
+++ proxy.ts
@@ -44,6 +44,6 @@ export const config = {
     "/login",
     "/register",
 
-    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    "/((?!_next/static|_next/image|favicon.ico|icon\\.svg|icon-[0-9]+\\.png|opengraph-image|twitter-image|sitemap\\.xml|robots\\.txt).*)",
   ],
 };
